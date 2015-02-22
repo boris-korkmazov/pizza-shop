690ce7f327b3598aef04a0a59bfdd2ca77f7af7b
boriz <boriskorkmazov1989@gmail.com>
2015-02-22 19:58:17 +0300

    play with localStorage

--- public/app.js
+++ public/app.js
@@ -1,3 +1,9 @@
- var somthing =  function(){
-    alert("Hello Evrevone")
-  }
+var somthing =  function(){
+    var x = window.localStorage.getItem('aaa')
+    if(! x){
+      window.localStorage.setItem('aaa', 555)
+    }
+
+    alert(window.localStorage.getItem('aaa'))  
+
+}
