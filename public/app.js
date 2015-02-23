fd884a0a3285dcf9703a4e698b80f638d5811e97
boriz <boriskorkmazov1989@gmail.com>
2015-02-23 12:33:11 +0300

    add cart itms

--- public/app.js
+++ public/app.js
@@ -1,17 +1,24 @@
-var somthing =  function(){
+(function(_this){
+  _this.somthing =  function(){
+    localStorage.clear()
+  }
 
-    var x = window.localStorage.getItem('ccc')
-    
-    x = x*1 + 1
+  _this.addToCart = function(id){
+    var key = 'product_' + id
+    var x = localStorage.getItem(key);
+    x = x*1 + 1;
+    localStorage.setItem(key, x);
 
-    
-    window.localStorage.setItem('ccc', x)
+    alert("Items in your cart: " + cartGetNumberOfItems())
+  }
 
-    alert(x)  
-
-}
-
-
-var addToCart = function(id){
-  alert("Added to cart")
-}
+  cartGetNumberOfItems = function(){
+    var sum = 0
+    for(var key in localStorage){
+      if(key.indexOf('product_')==0){
+        sum += parseInt(localStorage[key])
+      }
+    }
+    return sum
+  }
+})(window)
