899271e656bf4a25e8a11599f14533a6efb2a871
boriz <boriskorkmazov1989@gmail.com>
2015-02-23 13:09:59 +0300

    Update orders field

--- public/app.js
+++ public/app.js
@@ -8,11 +8,11 @@
     var x = localStorage.getItem(key);
     x = x*1 + 1;
     localStorage.setItem(key, x);
-
+    updateOrdersInput()
     alert("Items in your cart: " + cartGetNumberOfItems())
   }
 
-  cartGetNumberOfItems = function(){
+  var cartGetNumberOfItems = function(){
     var sum = 0
     for(var key in localStorage){
       if(key.indexOf('product_')==0){
@@ -21,4 +21,21 @@
     }
     return sum
   }
+
+  var updateOrdersInput = function(){
+     var order = cartGetOrders()
+     $('#order').val(order)
+  } 
+
+  var cartGetOrders = function(){
+    var str = ''
+    for(var key in localStorage){
+      if(key.indexOf('product_')==0){
+        str += key + "=" +localStorage[key]+ ','
+      }
+    }
+    return str
+  }
+
+
 })(window)
