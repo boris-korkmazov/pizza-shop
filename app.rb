7fd02f6b3706bc979bfeb4adeda7d2f541ccdded
boriz <boriskorkmazov1989@gmail.com>
2015-02-24 08:13:52 +0300

    add view for cart, handle order line

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+def parse_order_line(string)
+  array = string.split(',')
+  return_array= []
+  array.each{|item|
+    i = item.split('=')
+    return_array.push([i[0].split('_')[1], i[1]])
+  }
+  return_array
+end
+
 set :database, "sqlite3:pizzashop.sqlite"
 
 
@@ -20,5 +30,10 @@ get '/about' do
 end
 
 post '/cart' do
-  erb "hello"
+  @order= parse_order_line params[:order]
+  @order.map! do |item|
+    item[0] = Product.find(item[0])
+    item
+  end
+  erb :cart
 end
